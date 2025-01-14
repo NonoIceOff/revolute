cdf0ecd91a43191f8ff51caf118f6f73c6a97dfa
NonoIceOff <nonocraft28@gmail.com>
2025-01-14 16:57:31 +0100

    partie de story 10

--- routes/schemas.py
+++ routes/schemas.py
@@ -13,15 +13,12 @@ class CreateAccount(BaseModel):
     balance: float
     is_principal: bool
     is_closed: bool
-    creation_date: date
 
 class CreateTransactions(BaseModel):
     account_by_id: int
     account_to_id: int
     balance: float
     motif: str
-    creation_date: date
-    is_chancelled: bool
 
 class CreateDeposits(BaseModel):
     account_id: int
